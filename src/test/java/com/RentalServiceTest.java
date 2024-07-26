b7f716e69c44e6aa522ebf420ca3098522b900c9
OlafCoin001 <olafcoin001@gmail.com>
2024-07-26 11:48:14 -0500

    Update for all test cases

--- src/test/java/com/RentalServiceTest.java
+++ src/test/java/com/RentalServiceTest.java
@@ -42,4 +42,89 @@ public class RentalServiceTest {
         });
         assertEquals("Discount percent must be between 0 and 100.", exception.getMessage());
     }
+
+    // New test cases based on the provided scenarios
+    @Test
+    public void testScenario1() throws ParseException {
+        Date checkoutDate = dateFormat.parse("9/3/15");
+        Exception exception = assertThrows(InvalidRentalException.class, () -> {
+            rentalService.checkout("JAKR", 5, 101, checkoutDate);
+        });
+        assertEquals("Discount percent must be between 0 and 100.", exception.getMessage());
+    }
+
+    @Test
+    public void testScenario2() throws ParseException {
+        Date checkoutDate = dateFormat.parse("7/2/20");
+        try {
+            RentalAgreement agreement = rentalService.checkout("LADW", 3, 10, checkoutDate);
+            assertNotNull(agreement);
+            assertEquals("LADW", agreement.getTool().getCode());
+            assertEquals(3, agreement.getRentalDays());
+            assertEquals(10, agreement.getDiscountPercent());
+            assertEquals(checkoutDate, agreement.getCheckoutDate());
+        } catch (InvalidRentalException e) {
+            fail("Exception should not be thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testScenario3() throws ParseException {
+        Date checkoutDate = dateFormat.parse("7/2/15");
+        try {
+            RentalAgreement agreement = rentalService.checkout("CHNS", 5, 25, checkoutDate);
+            assertNotNull(agreement);
+            assertEquals("CHNS", agreement.getTool().getCode());
+            assertEquals(5, agreement.getRentalDays());
+            assertEquals(25, agreement.getDiscountPercent());
+            assertEquals(checkoutDate, agreement.getCheckoutDate());
+        } catch (InvalidRentalException e) {
+            fail("Exception should not be thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testScenario4() throws ParseException {
+        Date checkoutDate = dateFormat.parse("9/3/15");
+        try {
+            RentalAgreement agreement = rentalService.checkout("JAKD", 6, 0, checkoutDate);
+            assertNotNull(agreement);
+            assertEquals("JAKD", agreement.getTool().getCode());
+            assertEquals(6, agreement.getRentalDays());
+            assertEquals(0, agreement.getDiscountPercent());
+            assertEquals(checkoutDate, agreement.getCheckoutDate());
+        } catch (InvalidRentalException e) {
+            fail("Exception should not be thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testScenario5() throws ParseException {
+        Date checkoutDate = dateFormat.parse("7/2/15");
+        try {
+            RentalAgreement agreement = rentalService.checkout("JAKR", 9, 0, checkoutDate);
+            assertNotNull(agreement);
+            assertEquals("JAKR", agreement.getTool().getCode());
+            assertEquals(9, agreement.getRentalDays());
+            assertEquals(0, agreement.getDiscountPercent());
+            assertEquals(checkoutDate, agreement.getCheckoutDate());
+        } catch (InvalidRentalException e) {
+            fail("Exception should not be thrown: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testScenario6() throws ParseException {
+        Date checkoutDate = dateFormat.parse("7/2/20");
+        try {
+            RentalAgreement agreement = rentalService.checkout("JAKR", 4, 50, checkoutDate);
+            assertNotNull(agreement);
+            assertEquals("JAKR", agreement.getTool().getCode());
+            assertEquals(4, agreement.getRentalDays());
+            assertEquals(50, agreement.getDiscountPercent());
+            assertEquals(checkoutDate, agreement.getCheckoutDate());
+        } catch (InvalidRentalException e) {
+            fail("Exception should not be thrown: " + e.getMessage());
+        }
+    }
 }
